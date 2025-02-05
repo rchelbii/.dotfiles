fe996197f6af44ced4198f4efd33e61b2123e1ed
rchelbii <ryadhchalbi0@gmail.com>
2025-02-05 23:29:18 +0100

    neovim cursor!

--- nvim/init.lua
+++ nvim/init.lua
@@ -52,3 +52,45 @@ require "custom.default-nvdash"
 -- zig-lsp configs
 
 vim.g.zig_fmt_autosave = true
+
+-- Set highlight groups
+
+-- require("themes").setup {
+--   integrations = {
+--     native_lsp = { enabled = true },
+--     treesitter = true,
+--   },
+--   highlight_overrides = {
+--     all = function(colors)
+--       return {
+--         Cursor = { bg = colors.surface1, fg = "NONE" }, -- Keep text visible
+--         iCursor = { bg = colors.blue, fg = "NONE" },    -- Use Catppuccin blue for insert mode
+--       }
+--     end,
+--   },
+-- }
+
+-- Set cursor styles
+-- vim.opt.guicursor = {
+--   "n-v-c:block-Cursor",
+--   "i:ver100-iCursor",
+--   "n-v-c:blinkon0",
+--   "i:blinkwait10",
+-- }
+--
+--
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    -- This makes the cursor's foreground inherit the underlying text color
+    -- while setting its background to a color of your choosing (here, using colors from Catppuccin).
+    vim.api.nvim_set_hl(0, "Cursor", { fg = "NONE", bg = "#e1e1e1" })
+  end,
+})
+
+-- set cursor
+vim.opt.guicursor = "n-v-c:block-Cursor"
+vim.opt.guicursor:append "i:ver100-iCursor"
+vim.opt.guicursor:append "n-v-c:blinkon0"
+vim.opt.guicursor:append "i:blinkwait10"
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,4 +4,5 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
+
 o.scrolloff = 8
